bed8e6ce0e227ecf8adb040558f4db0bd8b53276
leotn03 <leo.tn03@icloud.com>
2024-01-09 10:23:47 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Leo Torres Neyra on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
