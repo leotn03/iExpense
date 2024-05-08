a46e15eb23aba69ec99470991757912e7c13cc0d
leotn03 <leo.tn03@icloud.com>
2024-05-07 21:32:03 -0500

    Created the iExpense app

---
+++ iExpense/AddView.swift
@@ -0,0 +1,61 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Leo Torres Neyra on 9/1/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    @State private var title = "Add new expense"
+
+    var expenses: Expenses
+    let types = ["Business", "Personal"]
+    private let currencyCode = Locale.current.currency?.identifier ?? "USD"
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+
+                TextField("Amount", value: $amount, format: .currency(code: currencyCode))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle($title)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save") {
+                        let item = ExpenseItem(name: name, type: type, amount: amount)
+                        expenses.items.append(item)
+                        dismiss()
+                    }
+                }
+                
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+            }
+            .navigationBarBackButtonHidden()
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
+
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,18 +7,97 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
         }
-        .padding()
+    }
+
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+
+        items = []
+    }
+}
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+
+    //@State private var showingAddExpense = false
+    
+    let types = ["Business", "Personal"]
+    private let currencyCode = Locale.current.currency?.identifier ?? "USD"
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach (types, id: \.self) { type in
+                    Section {
+                        ForEach(expenses.items) { item in
+                            if item.type == type {
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text(item.name)
+                                            .font(.headline)
+                                        
+                                        Text(item.type)
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    Text(item.amount, format: .currency(code: currencyCode))
+                                        .foregroundStyle(item.amount <= 5 ? .green : item.amount <= 10 ? .blue : item.amount <= 100 ? .orange : .red)
+                                }
+                            }
+                        }
+                        .onDelete(perform: removeItems)
+                    }header: {
+                        Text(type.uppercased())
+                    }
+                }
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                /** Para mostrar la ventana de agregar gastos como una hoja */
+                // Button("Add Expense", systemImage: "plus") {
+                //showingAddExpense = true
+                //}
+                
+                /** Para agregar gastos como una nueva ventana */
+                NavigationLink(destination: AddView(expenses: expenses)) {
+                    Image(systemName: "plus")
+                }
+            }
+            /** Para mostrar la ventana de agregar gastos como una hoja */
+            //.sheet(isPresented: $showingAddExpense) {
+            //    AddView(expenses: expenses)
+            //}
+        }
+        
+    }
+
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
 #Preview {
     ContentView()
 }
+
